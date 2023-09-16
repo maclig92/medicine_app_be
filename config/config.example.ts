bc128b47c8a7dc6620b403733976827473a9b9ad
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-09-16 15:44:07 +0200

    init commit

---
+++ config/config.example.ts
@@ -0,0 +1,12 @@
+/**
+
+export const config = {
+    dbHost: 'localhost',
+    dbUser: 'user',
+    dbPassword: 'password',
+    dbDatabase: 'database',
+    corsOrigin: 'http://localhost:3000',
+    serverPort: 3001,
+};
+
+ */
---
+++ index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+import { handleError } from './utils/errors';
+import { rateLimit } from 'express-rate-limit';
+import { config } from './config/config';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: config.corsOrigin,
+  }),
+);
+app.use(express.json());
+app.use(
+  rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
+  }),
+);
+
+app.use(handleError);
+
+app.listen(config.serverPort, '0.0.0.0', () => {
+  console.log(`listening on localhost:${config.serverPort}`);
+});
