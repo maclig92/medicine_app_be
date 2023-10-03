04e20160ff81a9bcedd8f585f925022d19dfa594
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-10-03 18:54:15 +0200

    ann endpoint to handle asigning prescription to med

--- routers/prescription.router.ts
+++ routers/prescription.router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { PrescriptionRepository } from '../records/prescription.repository';
+import { PrescriptionEntity } from '../types';
 
 export const prescriptionRouter = Router();
 
@@ -16,4 +17,22 @@ prescriptionRouter
   })
   .get('/:id', async (req, res) => {
     res.json(await PrescriptionRepository.getOne(req.params.id));
+  })
+  .post('/', async (req, res) => {
+    const { prescriptionNumber, issueDate, isYearly, isAntibiotic } =
+      req.body as PrescriptionEntity;
+
+    const insertedId = await PrescriptionRepository.insertOne({
+      prescriptionNumber: Number(prescriptionNumber),
+      issueDate: new Date(issueDate),
+      isYearly,
+      isAntibiotic,
+    });
+
+    return res.json(insertedId);
+  })
+  .patch('/:id/:medicineId', async (req, res) => {
+    const { id, medicineId } = req.params;
+    await PrescriptionRepository.assignMedicineToPrescription(id, medicineId);
+    res.json(req.params);
   });
