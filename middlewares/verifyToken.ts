3fe727889b64f10585cbef23347cc28567090243
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-10-18 14:07:31 +0200

    move verifyToken to middleware catalogue

---
+++ middlewares/verifyToken.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+// export interface TokenRequest extends Request {
+//   userId?: string;
+// }
+
+export const verifyToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized, token missing' });
+  }
+
+  try {
+    const decoded: JwtPayload | string = jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+    );
+
+    if (typeof decoded === 'string')
+      return res.status(401).json({ message: 'Unauthorized' });
+
+    req.userId = decoded.userId as string;
+
+    //    console.log(req.userId);
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Unauthorized', err });
+  }
+};
