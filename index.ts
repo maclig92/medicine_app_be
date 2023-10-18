db007af362f658f411ba432e17ac230d2a62f0ed
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-10-18 14:07:13 +0200

    fix problem with extending Request

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import { config } from './config/config';
 import { medicineRouter } from './routers/medicine.router';
 import { prescriptionRouter } from './routers/prescription.router';
 import { authRouter } from './routers/auth.router';
-import { verifyToken } from './utils/verifyToken';
+import { verifyToken } from './middlewares/verifyToken';
 // import './utils/db';
 
 require('dotenv').config();
@@ -21,12 +21,12 @@ app.use(
   }),
 );
 app.use(express.json());
-app.use(
-  rateLimit({
-    windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
-  }),
-);
+//app.use(
+//  rateLimit({
+//    windowMs: 5 * 60 * 1000, // 5 minutes
+//    max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
+//  }),
+//);
 
 app.use('/', authRouter);
 
