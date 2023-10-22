c911d651b2c0a199e4665b1eabd1c3c8420099ef
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-10-22 18:07:27 +0200

    change size of qrcode

--- utils/generateQRCode.ts
+++ utils/generateQRCode.ts
@@ -2,7 +2,7 @@ import QRCode from 'qrcode';
 
 export const generateQrCode = async (data: string) => {
   try {
-    const qr = await QRCode.toDataURL(data);
+    const qr = await QRCode.toDataURL(data, { width: 280 });
 
     // console.log(qr);
     return qr;
