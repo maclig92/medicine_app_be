287c57a07bf23cdecc46c8394f4929507cabba95
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-10-24 13:50:57 +0200

    remove console.logs

--- utils/generateQRCode.ts
+++ utils/generateQRCode.ts
@@ -4,7 +4,6 @@ export const generateQrCode = async (data: string) => {
   try {
     const qr = await QRCode.toDataURL(data, { width: 280 });
 
-    // console.log(qr);
     return qr;
   } catch (e) {
     console.error(e);
