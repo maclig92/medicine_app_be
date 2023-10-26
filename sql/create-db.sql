7c9dbe0f5c1a0ae46858f0e399daeed1393ac04a
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-10-26 13:02:28 +0200

    update create-db.sql

--- sql/create-db.sql
+++ sql/create-db.sql
@@ -3,19 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 18 Paź 2023, 22:06
+-- Czas generowania: 26 Paź 2023, 13:01
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Baza danych: `medicine-app`
@@ -45,14 +39,16 @@ CREATE TABLE `medicine` (
 --
 
 INSERT INTO `medicine` (`id`, `name`, `form`, `numberOfDailyDoses`, `doseUnit`, `doseQuantity`, `startDate`, `endDate`, `note`, `ownerId`) VALUES
-('C3K0S921ny', 'Augmentin', 'tabletki', 2, 'tabletka', '1.000', '2023-10-09', '2023-10-15', 'antybiotyk, co 12 godzin', 'e5rjIxBXr0'),
-('hfyijQbFEk', 'Atoris', 'tabletki', 1, 'tabletka', '1.000', NULL, NULL, 'wieczorem', 'e5rjIxBXr0'),
-('I5RXfk6SMJ', 'Vessel Due F', 'kapsułki', 2, 'wziew', '1.000', '2023-10-01', '0000-00-00', 'na żyły', 'e5rjIxBXr0'),
-('pKxoWapjR_', 'Fostex', 'aerozol in', 2, 'wziew', '2.000', '0000-00-00', '0000-00-00', '', 'e5rjIxBXr0'),
-('QlUsgqmh-Z', 'Letrox 50', 'tabletki', 1, 'tabletka', '1.000', NULL, NULL, 'na czczo', 'e5rjIxBXr0'),
-('UuU9AG-FTQ', 'Acard', 'tabletki', 1, 'tabl', '1.000', '0000-00-00', '0000-00-00', '', 'e5rjIxBXr0'),
-('v1qv6UlWjm', 'Bisocard', 'tabletki', 1, 'tabletka', '1.000', '2023-09-26', '2023-09-29', 'na serce', 'e5rjIxBXr0'),
-('XWqmzXJpO4', 'Fostex', 'aerozol', 2, 'wziew', '2.000', '2023-09-01', '2024-04-25', '', 'e5rjIxBXr0');
+('96VoUZ7zGI', 'DicloDuo Combi', 'kapsułki', 2, 'kapsułka', '1.000', '0000-00-00', '0000-00-00', '', 'TVP8eMlrGx'),
+('C3K0S921ny', 'Augmentin', 'tabletki', 2, 'tabletka', '1.000', '2023-10-09', '2023-10-15', 'antybiotyk, co 12 godzin', 'TVP8eMlrGx'),
+('hfyijQbFEk', 'Atoris', 'tabletki', 1, 'tabletka', '1.000', NULL, NULL, 'wieczorem', 'TVP8eMlrGx'),
+('I5RXfk6SMJ', 'Vessel Due F', 'kapsułki', 2, 'wziew', '1.000', '2023-10-01', '0000-00-00', 'na żyły', 'TVP8eMlrGx'),
+('pKxoWapjR_', 'Fostex', 'aerozol in', 2, 'wziew', '2.000', '0000-00-00', '0000-00-00', '', 'TVP8eMlrGx'),
+('QlUsgqmh-Z', 'Letrox 50', 'tabletki', 1, 'tabletka', '1.000', NULL, NULL, 'na czczo', 'TVP8eMlrGx'),
+('SiZlpKmMDY', 'Enterol', 'saszetki', 2, 'saszetka', '250.000', '2023-10-22', '2023-10-27', 'probiotyk', 'TVP8eMlrGx'),
+('UuU9AG-FTQ', 'Acard', 'tabletki', 1, 'tabl', '1.000', '0000-00-00', '0000-00-00', '', 'TVP8eMlrGx'),
+('v1qv6UlWjm', 'Bisocard', 'tabletki', 1, 'tabletka', '1.000', '2023-09-26', '2023-09-29', 'na serce', 'TVP8eMlrGx'),
+('XWqmzXJpO4', 'Fostex', 'aerozol', 2, 'wziew', '2.000', '2023-09-01', '2024-04-25', '', 'TVP8eMlrGx');
 
 -- --------------------------------------------------------
 
@@ -76,18 +72,22 @@ INSERT INTO `medicine_prescription` (`id`, `prescriptionId`, `medicineId`) VALUE
 (3, 't35T0wEiD1', NULL),
 (4, 'qWeRtY9292', 'hfyijQbFEk'),
 (8, '123asd123w', 'I5RXfk6SMJ'),
-(9, 't9D5rvf8a4', NULL),
-(10, 't9D5rvf8a4', NULL),
+(9, NULL, NULL),
+(10, NULL, NULL),
 (11, NULL, 'C3K0S921ny'),
 (12, '0hQXy_wA1_', NULL),
 (13, '0hQXy_wA1_', NULL),
 (14, '0hQXy_wA1_', 'UuU9AG-FTQ'),
 (15, '0hQXy_wA1_', 'v1qv6UlWjm'),
 (16, '0hQXy_wA1_', 'hfyijQbFEk'),
-(17, '7k3P0yCRls', 'UuU9AG-FTQ'),
-(18, '7k3P0yCRls', 'C3K0S921ny'),
+(17, NULL, 'UuU9AG-FTQ'),
+(18, NULL, 'C3K0S921ny'),
 (19, 'DiuEGIINzF', 'pKxoWapjR_'),
-(20, 'bBjli-ZVfZ', 'I5RXfk6SMJ');
+(20, 'bBjli-ZVfZ', 'I5RXfk6SMJ'),
+(21, 'lc5N8jX1-v', 'SiZlpKmMDY'),
+(22, 'lc5N8jX1-v', 'SiZlpKmMDY'),
+(23, '25MvMOibhU', 'I5RXfk6SMJ'),
+(24, '25MvMOibhU', 'hfyijQbFEk');
 
 -- --------------------------------------------------------
 
@@ -97,7 +97,7 @@ INSERT INTO `medicine_prescription` (`id`, `prescriptionId`, `medicineId`) VALUE
 
 CREATE TABLE `prescription` (
   `id` varchar(10) NOT NULL,
-  `prescriptionNumber` smallint(4) UNSIGNED NOT NULL,
+  `prescriptionNumber` varchar(4) NOT NULL DEFAULT '',
   `issueDate` date DEFAULT NULL,
   `isYearly` tinyint(1) NOT NULL DEFAULT 0,
   `isAntibiotic` tinyint(1) NOT NULL DEFAULT 0,
@@ -109,19 +109,22 @@ CREATE TABLE `prescription` (
 --
 
 INSERT INTO `prescription` (`id`, `prescriptionNumber`, `issueDate`, `isYearly`, `isAntibiotic`, `ownerId`) VALUES
-('0hQXy_wA1_', 1001, '2023-10-02', 0, 0, 'e5rjIxBXr0'),
-('123asd123w', 4365, '2023-09-30', 1, 0, 'e5rjIxBXr0'),
-('7k3P0yCRls', 1001, '2023-01-02', 0, 1, 'e5rjIxBXr0'),
-('bBjli-ZVfZ', 9999, '2023-10-03', 1, 0, 'e5rjIxBXr0'),
-('DiuEGIINzF', 6547, '2023-12-01', 0, 1, 'e5rjIxBXr0'),
-('djSkvkv2jd', 9205, '2023-10-01', 0, 0, 'e5rjIxBXr0'),
-('qWeRtY9292', 2233, '2023-09-15', 0, 0, 'e5rjIxBXr0'),
-('StYBNh3dXd', 5673, '2023-09-25', 0, 0, 'e5rjIxBXr0'),
-('t35T0wEiD1', 4985, '2023-08-12', 1, 0, 'e5rjIxBXr0'),
-('t9D5rvf8a4', 9999, '2023-10-03', 1, 0, 'e5rjIxBXr0'),
-('Wv0rawc2J8', 4598, '2023-12-01', 1, 0, 'e5rjIxBXr0'),
-('wWs23Sa98z', 2313, '2023-10-02', 1, 0, 'e5rjIxBXr0'),
-('Zzwr4hZJdD', 9999, '2023-10-03', 1, 0, 'e5rjIxBXr0');
+('0hQXy_wA1_', '1001', '2023-10-02', 0, 0, 'TVP8eMlrGx'),
+('123asd123w', '4365', '2023-09-30', 1, 0, 'TVP8eMlrGx'),
+('25MvMOibhU', '0001', '2023-12-31', 0, 0, 'TVP8eMlrGx'),
+('5wE00LiWdU', '4093', '2023-01-01', 0, 0, 'TVP8eMlrGx'),
+('Ah5PzJh8_N', '9998', '2023-01-31', 0, 0, 'TVP8eMlrGx'),
+('bBjli-ZVfZ', '9999', '2023-10-03', 1, 0, 'TVP8eMlrGx'),
+('DiuEGIINzF', '6547', '2023-12-01', 0, 1, 'TVP8eMlrGx'),
+('djSkvkv2jd', '9205', '2023-10-01', 0, 0, 'TVP8eMlrGx'),
+('lc5N8jX1-v', '2345', '2023-10-22', 1, 0, 'TVP8eMlrGx'),
+('m3Z_6udvhA', '0320', '2023-10-24', 0, 1, 'TVP8eMlrGx'),
+('qWeRtY9292', '2233', '2023-09-15', 0, 0, 'TVP8eMlrGx'),
+('RJc9342Fvv', '1347', '2023-12-31', 0, 1, 'TVP8eMlrGx'),
+('StYBNh3dXd', '5673', '2023-09-25', 0, 0, 'TVP8eMlrGx'),
+('t35T0wEiD1', '4985', '2023-08-12', 1, 0, 'TVP8eMlrGx'),
+('Wv0rawc2J8', '4598', '2023-12-01', 1, 0, 'TVP8eMlrGx'),
+('wWs23Sa98z', '2313', '2023-10-02', 1, 0, 'TVP8eMlrGx');
 
 -- --------------------------------------------------------
 
@@ -133,7 +136,7 @@ CREATE TABLE `user` (
   `id` varchar(10) NOT NULL,
   `username` varchar(25) NOT NULL,
   `email` varchar(40) NOT NULL,
-  `PESELnumber` int(11) UNSIGNED DEFAULT NULL,
+  `peselNumber` varchar(100) DEFAULT NULL,
   `password` varchar(60) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
@@ -143,8 +146,9 @@ CREATE TABLE `user` (
 -- Zrzut danych tabeli `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `email`, `PESELnumber`, `password`, `createdAt`, `updatedAt`) VALUES
-('e5rjIxBXr0', 'test', 'test@example.com', NULL, '$2b$10$A.6qzPuacn4YVlLTj3yire3vD0wojZ/Kz9rVVmzkm35KC0pOeoKAi', '2023-10-03 18:02:38', '2023-10-03 18:02:38'),
+INSERT INTO `user` (`id`, `username`, `email`, `peselNumber`, `password`, `createdAt`, `updatedAt`) VALUES
+('test_id', 'username', 'email@email.com', '{\"encrypted\":\"8865148a457861318d92fc491a87d960\",\"ivHex\":\"9e7f4be4fc351435b94c5d2ba38d8bc8\"}', '$2b$10$7jsqJkGvSBrimn3VIOqeleJUuYx/iTPC2h1zgbdpj2UFdJGOzEVaS', '2023-10-26 10:54:38', '2023-10-26 10:54:38'),
+('TVP8eMlrGx', 'test', 'test@example.pl', '{\"encrypted\":\"891772250593b7be9f12ba55c62ee518\",\"ivHex\":\"55d6399801ace53c6eee62f73881df2b\"}', '$2b$10$roYwRbAbGjylGIbkN3f8Hum5/hWfZiqXh1T7q.t8phuHyDJuFTM1.', '2023-10-24 19:09:27', '2023-10-24 19:09:37');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -179,7 +183,7 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `PESELnumber` (`PESELnumber`);
+  ADD UNIQUE KEY `PESELnumber` (`peselNumber`) USING BTREE;
 
 --
 -- AUTO_INCREMENT dla zrzuconych tabel
@@ -189,7 +193,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT dla tabeli `medicine_prescription`
 --
 ALTER TABLE `medicine_prescription`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Ograniczenia dla zrzutów tabel
@@ -199,7 +203,7 @@ ALTER TABLE `medicine_prescription`
 -- Ograniczenia dla tabeli `medicine`
 --
 ALTER TABLE `medicine`
-  ADD CONSTRAINT `FK_medicine_user` FOREIGN KEY (`ownerId`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `FK_medicine_user` FOREIGN KEY (`ownerId`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 --
 -- Ograniczenia dla tabeli `medicine_prescription`
@@ -212,9 +216,5 @@ ALTER TABLE `medicine_prescription`
 -- Ograniczenia dla tabeli `prescription`
 --
 ALTER TABLE `prescription`
-  ADD CONSTRAINT `FK_prescription_user` FOREIGN KEY (`ownerId`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `FK_prescription_user` FOREIGN KEY (`ownerId`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
